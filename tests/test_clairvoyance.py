a0da1279dd07b6705c6d55f4378d28b82075145f
niklastypes <baiern@proton.me>
2026-05-10 17:20:35 +0200

    test: remove version test that breaks on version bump

--- tests/test_clairvoyance.py
+++
@@ -1,8 +0,0 @@
-"""Tests package initialization."""
-
-from clairvoyance import __version__
-
-
-def test_version_is_set() -> None:
-    """Test that version is properly defined."""
-    assert __version__ == "0.1.0"
